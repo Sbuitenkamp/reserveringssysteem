fa2783aaa37f3fb2f7770140ed067cd610b52f0c
Teshale Noorman <tnoorman@zoho.com>
2019-05-29 13:53:35 +0200

    setup for authentication

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 const express = require('express');
+const server = express();
+const expressWs = require('express-ws')(server);
+const port = process.env.PORT || 3000;
 const session = require('express-session');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const morgan = require('morgan');
 const path = require('path');
 const fs = require('fs');
 const { Server: WebSocketServer } = require('ws');
 const db = require('./models/db');
 const wss = new WebSocketServer({ port: 40510 });
-const server = express();
-const port = process.env.PORT || 3000;
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+// const browserSync = require('browser-sync');
+// const bs = browserSync.create().init({ proxy: `localhost:${port}`, watch: true, files: '**/*', logSnippet: false });
+// const bsConnect = require('connect-browser-sync')(bs, { injectHead: true });
 
 // express initialisation
 server.listen(port, () => console.log(`Server listening at http://localhost:${port}`));
@@ -16,14 +24,33 @@ server.use(express.static(`${__dirname}/controllers`));
 server.use(express.static(`${__dirname}/styles`));
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
+server.use(cookieParser());
 server.use(session({
     secret: 'yrla is thicc af',
-    resave: true,
-    saveUninitialized: true
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 600000
+    }
 }));
+server.ws('/echo', (ws, req) => {
+    ws.on('message', (msg) => {
+        ws.send(msg);
+    })
+});
 
+server.use(morgan('dev'));
+
+// User will not have access to the environment unless they are signed in
+const sessionCheck = (req, res, next) => {
+    if(!req.session.user && !req.cookies.user_sid) {
+        console.log('you\'re not allowed here!!');
+        next();
+    }
+};
 // websocket
 wss.on('connection', ws => {
+    console.log('fuck');
     ws.on('message', async message => {
         let data;
         message = JSON.parse(message);
@@ -44,6 +71,13 @@ wss.on('connection', ws => {
                 data = await destroy(message);
                 ws.send(JSON.stringify(data));
                 break;
+            case 'authenticate':
+                ws.on('message', async message => {
+                    const auth = await message;
+                    ws.send(JSON.stringify(auth));
+                    console.log(`koeien in de sloot | ${auth} | paard in de hei`);
+                });
+                break;
             default:
                 throw new Error('database operation was not defined');
         }
@@ -52,11 +86,17 @@ wss.on('connection', ws => {
 
 // dynamic file serving
 server.get('/', (req, res) => res.redirect('/index'));
+server.get('/reservation-overview', sessionCheck, (req, res) => res.redirect('/'));
+
 server.get('/:path', (req, res) => {
     if (req.params.path.trim().toLowerCase().endsWith('.html')) return res.redirect(req.params.path.substring(0, req.params.path.length - 5));
     const url = path.join(`${__dirname}/views/${req.params.path}.ejs`);
-    if (fs.existsSync(url)) res.render(url);
-    else res.redirect('/page-not-found');
+    if (fs.existsSync(url)) {
+        res.render(url);
+    } else {
+        res.status(200);
+        res.redirect('/page-not-found')
+    }
 });
 
 async function create({ table, options }) {}
