5da49c60d42ec9ae6d4579466ddf38e32bfaa857
Sbuitenkamp <buitenkamp.developer@gmail.com>
2019-06-04 13:43:13 +0200

    hotfixes + login finally completed

--- controllers/index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('load', () => {
-    const loginBtn = document.getElementById('loginBtn');
-    console.log(loginBtn);
-
-    loginBtn.addEventListener('click', () => {
-        console.log(123);
-    })
-});
