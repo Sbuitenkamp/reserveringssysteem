a9e2ef8fe3448b5aa68e7b52ba5f213d5c81ba2e
Teshale Noorman <tnoorman@zoho.com>
2019-06-13 02:11:35 +0200

    Styling of the pop-up has been fixed, deleted the grid and instead used flexbox cause of inconveniences with grid. Also made some hotfixes, received some feedback from a senior developer, because of that the readability of the code has been improved.

--- server.js
+++ server.js
@@ -35,33 +35,16 @@ wss.on('connection', ws => {
         message = JSON.parse(message);
         switch (message.type) {
             case 'create':
-                data = await create(message).catch(e => {
-                    console.log(new Date() + '| an error occurred during the creation of one or more database entries:');
-                    console.error(e);
-                });
-                ws.send(JSON.stringify(data));
+                switchCreate(data, message)
                 break;
             case 'select':
-                data = await select(message).catch(e => {
-                    console.log(new Date() + '| an error occurred during the selection of one or more database entries:');
-                    console.error(e);
-                });
-                ws.send(JSON.stringify(data));
+                switchSelect(data, message);
                 break;
             case 'update':
-                await update(message).catch(e => {
-                    console.log(new Date() + '| an error occurred during the updating of one or more database entries:');
-                    console.error(e);
-                });
-                data = { update: true };
-                ws.send(JSON.stringify(data));
+                switchUpdate(data, message);
                 break;
             case 'delete':
-                data = await destroy(message).catch(e => {
-                    console.log(new Date() + '| an error occurred during the deletion of one or more database entries:');
-                    console.error(e);
-                });
-                ws.send(JSON.stringify(data));
+                switchDestroy(data, message)
                 break;
             default:
                 throw new Error('database operation was not defined (this is a user-made error)');
@@ -69,7 +52,7 @@ wss.on('connection', ws => {
     });
 });
 
-// dynamic file serving
+// Dynamic file serving //
 server.get('/', (req, res) => res.redirect('/index'));
 server.get('/:path', (req, res) => {
     if (req.params.path.trim().toLowerCase().endsWith('.html')) return res.redirect(req.params.path.substring(0, req.params.path.length - 5));
@@ -80,64 +63,150 @@ server.get('/:path', (req, res) => {
     else if (fs.existsSync(url)) res.render(url);
     else res.redirect('/page-not-found');
 });
+// Dynamic file serving //
 
-// posts for actions
+// Server post section //
 
-// authenticate the user who tries to login
+// Authenticate the user who attempts to login
 server.post('/authenticate', (req, res) => {
     const { username, password } = req.body;
     db.users.findOne({ where: { userName: username }}).then((user) => {
-        if (!user) {
-            console.log("Username or password is incorrect");
-            res.redirect('/');
-        } else {
-            bcrypt.compare(password, user.password, (err, result) => {
-                if (err) throw err;
-                if (!result) {
-                    console.log("Username or password is incorrect");
-                    res.redirect('/');
-                } else {
-                    req.session.user = {
-                        id: user.id,
-                        username: user.userName,
-                        superUser: user.superUser
-                    };
-                    res.redirect('/reservation-overview');
-                }
-            });
-        }
+        userExists(user, password. user.password);
     });
 });
 
-// logout
+// Logout
 server.post('/logout', (req, res) => {
     req.session.destroy();
     res.redirect('/index');
 });
 
 /* 
-    since we don't use partials in this project,
+    Since we don't use partials in this project,
     the pop-up partial will be retrieved in the view by a post request
 */
+
 server.post('/reservation-pop-up', (req, res) => {
     const html = res.render(path.join(`${__dirname}/models/reservation-pop-up.ejs`), { node: req.body });
     res.send(html);
 });
 
-// other functions
-function create({ table, options, values }) {
+// Server post section //
+
+/*
+    Functions have been put at the bottom of this server file for clean code:
+
+    - Basic functions
+    - CRUD functions
+    - Shortened the functions for switch
+    - Functions for authentication
+
+*/
+
+// Basic functions //
+
+const socketSend = (data) => {
+    ws.send(JSON.stringify(data));
+}
+
+// Basic functions //
+
+// CRUD functions //
+const create = ({ table, options, values }) => {
     console.log(`${new Date()} | creating entry in ${table}...`);
     return db[table].create(values, options);
 }
+
 function select({ table, options }) {
     console.log(`${new Date()} | fetching ${options.limit || 'all'} entries from ${table}...`);
     return db[table].findAll(options);
 }
+
 function update({ table, options, values }) {
     console.log(`${new Date()} | updating entries from ${table}...`);
     return db[table].update(values, options);
 }
+
 function destroy({ table, options }) {
     console.log(`${new Date()} | deleting entries from ${table}...`);
     return db[table].destroy(options);
+}
+// CRUD functions //
+
+// Shortened the functions for switch //
+const switchSelect = async (data, message) => {
+    data = await select(message).catch(e => {
+        console.log(new Date() + '| an error occurred during the selection of one or more database entries:');
+        console.log(e);
+    });
+    socketSend(data);
+}
+
+const switchCreate = async (data, message) => {
+    data = await create(message).catch(e => {
+        console.log(new Date() + '| an error occurred during the creation of one or more database entries:');
+        console.log(e);
+    });
+    socketSend(data);
+}
+
+const switchUpdate = async (data, message) => {
+    data = await update(message).catch(e => {
+        console.log(new Date() + '| an error occurred during the updating of one or more database entries:');
+        console.log(e);
+    });
+    data = { update: true };
+    socketSend(data);
+}
+
+const switchDestroy = async (data, message) => {
+    data = await destroy(message).catch(e => {
+        console.log(new Date() + '| an error occurred during the deletion of one or more database entries:');
+        console.log(e);
+    });
+    socketSend(data);
+}
+// Shortened the functions for switch //
+
+// Functions for authenticating the user for login //
+
+// This will check if the user exists in the database
+const userExists = (user, password, userPassword) => {
+    if(!user) {
+        console.log("Username or password is incorrect");
+        res.redirect('/');
+    } else {
+        validatePassword(password, userPassword)
+    }
+}
+
+/* 
+    This will use bcrypt.compare to see if the passwords match
+    If the passwords match, it will pass the result into validResult() 
+*/
+
+const validatePassword = (password, userPassword) => {
+    bcrypt.compare(password, userPassword), (err, result) => {
+        if (err) throw err;
+        validResult(result);
+    }
+}
+
+/*
+    This is for bcrypt.compare, since it takes 'err' and 'result' as callback
+    If there is no result, the user can't be logged in and had to try again
+*/
+
+const validResult = (result) => {
+    if(!result) {
+        console.log("Username or password is incorrect");
+        res.redirect('/');
+    } else {
+        req.session.user = {
+            id: user.id,
+            username: user.userName,
+            superUser: user.superUser
+        };
+        res.redirect('/reservation-overview');
+    }
 }
